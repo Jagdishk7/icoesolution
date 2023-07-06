543889b1bb83f51be42ece2b63871ab985ae0f3d
Jagdish Kushwaha <pwdacait7@gmail.com>
2023-07-06 15:19:51 +0530

    portfolio and about page added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Portfolio from './pages/Portfolio/Portfolio';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
       </BrowserRouter>
     </>
